2cab0ba110cf48d98d207186ad98b0fec5271006
Руслан <fantom203232@gmail.com>
2025-09-19 22:12:04 +0500

    Delete core directory

--- core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.shortcuts import render
-
-def home(request):
-    return render(request, "core/index.html")
-
-urlpatterns = [
-    path('', home, name='home'),
-]
